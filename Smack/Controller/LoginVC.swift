6e9a3000871075bf99eddb1c0f03e919b5d75b7b
Matt Tripodi <mtripodi1027@gmail.com>
2017-08-21 16:54:21 -0400

    Created LoginVC UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Matt Tripodi on 8/21/17.
+//  Copyright © 2017 Matthew Tripodi. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+	
+	//Outlets
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+	
+	@IBAction func closePressed(_ sender: Any) {
+		dismiss(animated: true, completion: nil)
+	}
+	
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Matt Tripodi on 8/21/17.
+//  Copyright © 2017 Matthew Tripodi. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
