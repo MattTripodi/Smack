cc853516623226192f35623c325faae29c9f6d47
Matt Tripodi <mtripodi1027@gmail.com>
2017-08-29 13:51:18 -0400

    Created MessageService and Channel Model.

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,16 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Matt Tripodi on 8/29/17.
+//  Copyright © 2017 Matthew Tripodi. All rights reserved.
+//
+
+import Foundation
+
+struct Channel: Decodable {
+	public private(set) var channelTitle: String!
+	public private(set) var channelDescription: String!
+	public private(set) var id: String!
+
+}
