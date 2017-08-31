41a8f393017c3b770a2b6d3e88ee4081e1d0b4c6
Matt Tripodi <mtripodi1027@gmail.com>
2017-08-31 12:01:32 -0400

    setup ChatVC TableView

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class ChatVC: UIViewController {
+class ChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 	
 	//IBOutlets
 	@IBOutlet weak var menuBtn: UIButton!
 	@IBOutlet weak var channelNameLbl: UILabel!
 	@IBOutlet weak var messageTxtBox: UITextField!
+	@IBOutlet weak var tableView: UITableView!
 	
 	
     override func viewDidLoad() {
@@ -21,6 +22,11 @@ class ChatVC: UIViewController {
 		view.bindToKeyboard()
 		let tap = UITapGestureRecognizer(target: self, action: #selector(ChatVC.handleTap))
 		view.addGestureRecognizer(tap)
+		
+		tableView.estimatedRowHeight = 80
+		tableView.rowHeight = UITableViewAutomaticDimension
+		tableView.delegate = self
+		tableView.dataSource = self
 		
 		menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
 		self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
@@ -88,9 +94,31 @@ class ChatVC: UIViewController {
 	func getMessages() {
 		guard let channelId = MessageService.instance.selectedChannel?.id else { return }
 		MessageService.instance.findAllMessagesForChannel(channelId: channelId) { (success) in
-			
+			if success {
+				self.tableView.reloadData()
+			}
 		}
 	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		if let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as? MessageCell {
+			let message = MessageService.instance.messages[indexPath.row]
+			cell.configureCell(message: message)
+			return cell
+		} else {
+			return UITableViewCell()
+		}
+	}
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return MessageService.instance.messages.count
+	}
+	
+	
 
 }
 
